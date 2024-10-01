2691d0018fc122e3b6f224bddb3056442a8716a3
anugrah74 <anuraghdwisaputra8@gmail.com>
2024-10-01 11:22:21 +0700

    Upload pertama

---
+++ resources/views/Guru/layout/app.blade.php
@@ -0,0 +1,10 @@
+@include('admin.layout.header')
+@include('admin.layout.side')
+
+<div class="container-fluid px-4">
+
+        @yield('konten')
+        </main>
+@include('admin.layout.footer')
+
+</div>
---
+++ resources/views/siswa/layout/app.blade.php
@@ -0,0 +1,10 @@
+@include('siswa.layout.header')
+@include('siswa.layout.side')
+
+<div class="container-fluid px-4">
+
+        @yield('konten')
+
+@include('siswa.layout.footer')
+
+</div>
